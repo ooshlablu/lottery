96afb0c30e75eed60f771c5617ee77b8fc08109f
ooshlablu <phagan@brewaces.com>
2021-02-04 12:21:11 -0500

    Now featuring modules

---
+++ src/lottery_actions.rs
@@ -0,0 +1,66 @@
+use rand::Rng;
+
+// I don't think there's a game in the U.S. that doesn't follow these rules.
+// There should be a a pool of numbers to pick from starting at 1
+// and ending at a high number, 'high_ball', with a number of 'draws'
+// to pick from that pool of numbers. There should also be a bonus draw,
+// with a pool of numbers starting at 1 and ending 'bonus_high_ball',
+// with a number of 'bonus_draws' to pick from that pool of numbers.
+pub struct LotteryRules {
+    pub high_ball: i32,
+    pub bonus_high_ball: i32,
+    pub draws: i32,
+    pub bonus_draws: i32,
+}
+
+// Pick a random number within the range created by 'high_ball'
+fn rand_pick(rand_max: i32) -> i32 {
+   rand::thread_rng().gen_range(1..=rand_max)
+}
+
+// Loop through and pick the amount of numbers we specify
+pub fn pick_em(high_ball: i32, draws: i32) -> Vec<i32> {
+    let mut picks = Vec::new();
+
+    let mut pick_num = 0;
+
+    // Pick the numbers
+    while pick_num < draws {
+        let pick = rand_pick(high_ball);
+
+        // It is only a valid pick if it does not
+        // match a number that already has been picked.
+        if !picks.contains(&pick) {
+            picks.push(pick);
+            pick_num += 1;
+        }
+    };
+
+    // Make them nice and in order
+    picks.sort();
+
+    picks
+}
+
+pub fn print_winners(winning_numbers: Vec<i32>, bonus_numbers: Vec<i32>) {
+    print!("Winning numbers are ");
+
+    // Loop through the winning and bonus numbers. Format the output
+    // with a '-' between the numbers, like how it would look on
+    // a printout from a lottery machine
+    for winning_number in winning_numbers.iter() {
+        if winning_number == winning_numbers.last().unwrap() {
+            print!("{} ", winning_number);
+        } else {
+            print!("{}-", winning_number);
+        };
+    }
+
+    for bonus_number in bonus_numbers.iter() {
+        if bonus_number == bonus_numbers.last().unwrap() {
+            println!("({})", bonus_number);
+        } else {
+            print!("({})-", bonus_number);
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-// I don't think there's a game in the U.S. that doesn't follow these rules
-// There should be a a pool of numbers to pick from starting at 1
-// and ending at a high number, 'high_ball', with a number of 'draws'
-// to pick from that pool of numbers. There should also a bonus draw,
-// with a pool of numbers starting at 1 and ending 'bonus_high_ball',
-// with a number of 'bonus_draws' to pick from that pool of numbers.
-struct LotteryRules {
-    high_ball: i32,
-    bonus_high_ball: i32,
-    draws: i32,
-    bonus_draws: i32,
-}
-
-use rand::Rng;
 use getopts::Options;
 use std::env;
 
+mod lottery_actions;
+
 fn main() {
+    // Set up the rules for the games
     // See https://www.megamillions.com/how-to-play
-    let megamillions = LotteryRules {
+    let megamillions = lottery_actions::LotteryRules {
         high_ball: 70,
         bonus_high_ball: 25,
         draws: 5,
@@ -25,7 +14,7 @@ fn main() {
     };
 
     // See https://www.masslottery.com/games/powerball/how-to-play
-    let powerball = LotteryRules {
+    let powerball = lottery_actions::LotteryRules {
         high_ball: 69,
         bonus_high_ball: 26,
         draws: 5,
@@ -52,7 +41,7 @@ fn main() {
     }
 
     // Default to megamillions, and switch to powerball if '-p' is called
-    let game: &LotteryRules = if matches.opt_present("p") {
+    let game: &lottery_actions::LotteryRules = if matches.opt_present("p") {
         &powerball
     } else {
         &megamillions
@@ -60,66 +49,14 @@ fn main() {
 
 
     // Pick the winners
-    let winning_numbers = pick_em(game.high_ball, game.draws);
-    let bonus_numbers = pick_em(game.bonus_high_ball, game.bonus_draws);
+    let winning_numbers = lottery_actions::pick_em(game.high_ball, game.draws);
+    let bonus_numbers = lottery_actions::pick_em(game.bonus_high_ball, game.bonus_draws);
 
-    print_winners(winning_numbers, bonus_numbers);
+    lottery_actions::print_winners(winning_numbers, bonus_numbers);
 }
 
 // Usage statement for getopts
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options]", program);
     print!("{}", opts.usage(&brief));
-}
-
-// Pick a random number within the range created by 'high_ball'
-fn rand_pick(rand_max: i32) -> i32 {
-   rand::thread_rng().gen_range(1..=rand_max)
-}
-
-// Loop through and pick the amount of numbers we specify
-fn pick_em(high_ball: i32, draws: i32) -> Vec<i32> {
-    let mut picks = Vec::new();
-
-    let mut pick_num = 0;
-
-    // Pick the numbers
-    while pick_num < draws {
-        let pick = rand_pick(high_ball);
-
-        // It is only a valid pick if it does not
-        // match a number that already has been picked.
-        if !picks.contains(&pick) {
-            picks.push(pick);
-            pick_num += 1;
-        }
-    };
-
-    // Make them nice and in order
-    picks.sort();
-    
-    picks
-}
-
-fn print_winners(winning_numbers: Vec<i32>, bonus_numbers: Vec<i32>) {
-    print!("Winning numbers are ");
-
-    // Loop through the winning and bonus numbers. Format the output
-    // with a '-' between the numbers, like how it would look on
-    // a printout from a lottery machine
-    for winning_number in winning_numbers.iter() {
-        if winning_number == winning_numbers.last().unwrap() {
-            print!("{} ", winning_number);
-        } else {
-            print!("{}-", winning_number);
-        };
-    }
-
-    for bonus_number in bonus_numbers.iter() {
-        if bonus_number == bonus_numbers.last().unwrap() {
-            println!("({})", bonus_number);
-        } else {
-            print!("({})-", bonus_number);
-        };
-    }
 }
